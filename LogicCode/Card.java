3f2bae9bc5ac8ed316b85e2cc8d90b327632854e
cmihran <charlie.mihran@gmail.com>
2014-02-05 13:11:50 -0800

    Added legal arguments check

--- LogicCode/Card.java
+++ LogicCode/Card.java
@@ -1,13 +1,28 @@
-
 public class Card {
 	int value;
 	String label;
 	String suit;
 	
 	public Card(int value, String label, String suit){
+		if(!isLegal(value, label suit)) throw new IllegalArgumentsException();
 		this.value = value;
 		this.label = label;
 		this.suit = suit;
+	}
+	
+	public boolean isLegal(int value, String label, String suit){
+		boolean retval = false;
+		//check value
+		if(value < 0 || value > 11) retval = false;
+		//check label
+		for(int i = 0; i < Deck.labels.length; i++){
+			if(Deck.lebals[i].equals(label)) retval = true;
+		}
+		//check suit
+		for(int i = 0; i < Deck.suits.length; i++){
+			if(Deck.suits[i].equals(label)) retval = true;
+		}
+		return retval;
 	}
 	
 	public int getValue(){
