2bd6f6c5fd85536a216864ee8dd3eede6bdd867f
cmihran <charlie.mihran@gmail.com>
2014-03-03 20:48:39 -0700

    Update Card.java

--- LogicCode/Card.java
+++ LogicCode/Card.java
@@ -3,63 +3,68 @@ public class Card {
 	String label;
 	String suit;
 	boolean avail;
-	
-	public Card(int value, String label, String suit){
-		if(!isLegal(value, label, suit)) throw new IllegalArgumentException();
+
+	public Card(int value, String label, String suit) {
+		if (!isLegal(value, label, suit))
+			throw new IllegalArgumentException();
 		this.value = value;
 		this.label = label;
 		this.suit = suit;
 		avail = true;
 	}
-	
-	public Card(int x){
-		if(x != -1) throw new IllegalArgumentException();
+
+	public Card(int x) {
+		if (x != -1)
+			throw new IllegalArgumentException();
 		value = 0;
 		label = Deck.labels[0];
 		suit = Deck.suits[0];
 		avail = false;
 	}
-	
-	public boolean isLegal(int value, String label, String suit){
+
+	public boolean isLegal(int value, String label, String suit) {
 		boolean retval = false;
-		//check value
-		if(value < 0 || value > 11) return false;
-		//check label
-		for(int i = 0; i < Deck.labels.length; i++){
-			if(Deck.labels[i].equals(label)) retval = true;
+		// check value
+		if (value < 0 || value > 11)
+			return false;
+		// check label
+		for (int i = 0; i < Deck.labels.length; i++) {
+			if (Deck.labels[i].equals(label))
+				retval = true;
 		}
-		//check suit
-		for(int i = 0; i < Deck.suits.length; i++){
-			if(Deck.suits[i].equals(label)) retval = true;
+		// check suit
+		for (int i = 0; i < Deck.suits.length; i++) {
+			if (Deck.suits[i].equals(label))
+				retval = true;
 		}
 		return retval;
 	}
-	
-	public boolean getAvail(){
+
+	public boolean getAvail() {
 		return avail;
 	}
-	
-	public void setAvail(boolean avail){
+
+	public void setAvail(boolean avail) {
 		this.avail = avail;
 	}
-	
-	public int getValue(){
+
+	public int getValue() {
 		return value;
 	}
-	
-	public String getLabel(){
+
+	public String getLabel() {
 		return label;
 	}
-	
-	public String getSuit(){
+
+	public String getSuit() {
 		return suit;
 	}
-	
-	public void printCard(){
+
+	public void printCard() {
 		System.out.println(this.toString());
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return label + " of " + suit;
 	}
 }
