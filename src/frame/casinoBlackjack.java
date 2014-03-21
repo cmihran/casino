02868cc473d9b9e50fc6954b2a0f39a94dfeacff
cmihran <charlie.mihran@gmail.com>
2014-03-21 09:25:51 -0700

    Moved back button

--- src/frame/casinoBlackjack.java
+++ src/frame/casinoBlackjack.java
@@ -1,3 +1,4 @@
+package frame;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -7,46 +8,41 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
+import logic.Deck;
+import logic.Hand;
+
 public class casinoBlackjack extends JComponent implements casinoInterface {
 	final int DISPLAY_WIDTH;
 	final int DISPLAY_HEIGHT;
-
+	final int buttonOffset = 10;
+	
 	BackButton buttonBack;
 	StayButton buttonStay;
 	HitButton buttonHit;
 
-	Deck deck = new Deck();
-	Hand player = new Hand("Player");
-	Hand dealer = new Hand("Dealer");
+	Deck deck;
+	Hand player;
+	Hand dealer;
+	
+	JLabel txt = new JLabel();
 
 	public casinoBlackjack(int width, int height) {
 		DISPLAY_WIDTH = width;
 		DISPLAY_HEIGHT = height;
 		init();
 	}
-
-	public void init() {
-		setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-
-		buttonBack = new BackButton();
-		buttonBack.setBounds(0, 0, casinoMain.bWidth, casinoMain.bHeight);
-		add(buttonBack);
-		buttonBack.setVisible(true);
+	
+	public void newCards(){
+		deck = new Deck();
+		player = new Hand("Player");
+		dealer = new Hand("Dealer");
 		
-		buttonStay = new StayButton();
-		//finish buttonBack.setBounds(0, 0, casinoMain.bWidth, casinoMain.bHeight);
-		add(buttonBack);
-		buttonBack.setVisible(true);
-
-		
-		//text
 		dealer.add(deck.getACard());
 		dealer.add(deck.getACard());
 		
 		player.add(deck.getACard());
 		player.add(deck.getACard());
 		
-		JLabel txt = new JLabel();
 		txt.setText("<html>" + dealer.toString() 
 				+ "<br>Dealer Sum: " + dealer.getSum()
 				+ "<br>" + player.toString()
@@ -57,6 +53,23 @@ public class casinoBlackjack extends JComponent implements casinoInterface {
 		add(txt);
 		txt.setBounds(100,100, 500,100);
 		txt.setVisible(true);
+	}
+
+	public void init() {
+		setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+		buttonBack = new BackButton();
+		System.out.println(casinoMain.height + " " + casinoMain.bHeight);
+		buttonBack.setBounds(0+buttonOffset, (casinoMain.height - 2*casinoMain.bHeight) -buttonOffset, casinoMain.bWidth, casinoMain.bHeight);
+		add(buttonBack);
+		buttonBack.setVisible(true);
+		
+		buttonStay = new StayButton();
+		//finish buttonBack.setBounds(0, 0, casinoMain.bWidth, casinoMain.bHeight);
+		add(buttonBack);
+		buttonBack.setVisible(true);
+
+		newCards();
 
 	}
 
@@ -67,6 +80,7 @@ public class casinoBlackjack extends JComponent implements casinoInterface {
 		}
 
 		public void actionPerformed(ActionEvent arg0) {
+			newCards();
 			casinoMain.modeStart();
 		}
 	}
@@ -92,6 +106,4 @@ public class casinoBlackjack extends JComponent implements casinoInterface {
 			
 		}
 	}
-	
-
 }
